18b70b1b7047eab74464001ea5cd032a8651edb9
Shubh Patel <shubhpatel891@gmail.com>
2024-01-18 12:23:07 +0300

    Added MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import connectDB from "./db/connectDB.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const DATABASE_URL = process.env.DATABASE_URL;
+
+connectDB(DATABASE_URL);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000!");
